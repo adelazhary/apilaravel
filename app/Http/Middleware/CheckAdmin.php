2e39422f48f460efbab7f2fcba8eb2038319e012
Adil omer <36453919+adelazhary@users.noreply.github.com>
2023-03-16 18:56:42 +0800

    .

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -7,6 +7,8 @@ use Closure;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
 
 class CheckAdmin
 {
@@ -19,28 +21,28 @@ class CheckAdmin
     public function handle($request, Closure $next)
     {
         $user = null;
-        // try {
-        //     $user = JWTAuth::parseToken()->authenticate();
-        // } catch (\Exception $e) {
-        //     if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
-        //         return $this -> returnError('E3001','INVALID_TOKEN');
-        //     } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
-        //         return $this -> returnError('E3001','EXPIRED_TOKEN');
-        //     } else {
-        //         return $this -> returnError('E3001','TOKEN_NOTFOUND');
-        //     }
-        // } catch (\Throwable $e) {
-        //     if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
-        //         return $this -> returnError('E3001','INVALID_TOKEN');
-        //     } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
-        //         return $this -> returnError('E3001','EXPIRED_TOKEN');
-        //     } else {
-        //         return $this -> returnError('E3001','TOKEN_NOTFOUND');
-        //     }
-        // }
+        try {
+            $user = App\Http\Middleware\JWTAuth::parseToken()->authenticate();
+        } catch (\Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                return $this -> returnError('E3001','INVALID_TOKEN');
+            } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return $this -> returnError('E3001','EXPIRED_TOKEN');
+            } else {
+                return $this -> returnError('E3001','TOKEN_NOTFOUND');
+            }
+        } catch (\Throwable $e) {
+            if ($e instanceof TokenInvalidException) {
+                return $this -> returnError('E3001','INVALID_TOKEN');
+            } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return $this -> returnError('E3001','EXPIRED_TOKEN');
+            } else {
+                return $this -> returnError('E3001','TOKEN_NOTFOUND');
+            }
+        }
 
-        // if (!$user)
-        // $this -> returnError(trans('Unauthenticated'));
-        // return $next($request);
+        if (!$user)
+        $this -> returnError(trans('Unauthenticated'));
+        return $next($request);
     }
 }
